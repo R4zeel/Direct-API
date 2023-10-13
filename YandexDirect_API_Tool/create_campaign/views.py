38147ec27f15ec19e3c8983c19847413d0a2599f
Tony <r4zeel@yandex.ru>
2023-10-13 20:39:03 +0300

    fixed bootstrap formset, added create ad_group and creative functions

--- YandexDirect_API_Tool/create_campaign/views.py
+++ YandexDirect_API_Tool/create_campaign/views.py
@@ -12,7 +12,7 @@ def index(request):
     formset = campaigns_formset(request.GET or None)
     if formset.is_valid():
         for form in formset:
-            campaigns_create.CreateCampaign(
+            campaign_id = campaigns_create.CreateCampaign(
                 form.cleaned_data['campaign_name'],
                 form.cleaned_data['cpm'],
                 form.cleaned_data['total_budget'],
@@ -21,6 +21,18 @@ def index(request):
                 form.cleaned_data['frequency_cap'],
                 form.cleaned_data['frequency_period'],
             ).create_campaign()
+            ad_group_id = campaigns_create.CreateAdGroup(
+                campaign_id,
+                0
+            ).create_ad_group()
+            creatives = (
+                campaigns_create.CreateHtmlCreatives(
+                    ad_group_id,
+                    'Dis18-soap'
+                )
+            )
+            creatives.get_creatives()
+            creatives.create_creatives()
     context = {'formset': formset}
     return render(request, template, context)
 
