264956f23a3aa5911cfd1968430c6e65892f698b
Tony <r4zeel@yandex.ru>
2023-10-13 20:06:28 +0300

    some template updates, implemented formsets

--- YandexDirect_API_Tool/create_campaign/forms.py
+++ YandexDirect_API_Tool/create_campaign/forms.py
@@ -4,4 +4,15 @@ from django import forms
 class CampaignSettingsForm(forms.Form):
     campaign_name = forms.CharField(label='Название РК')
     total_budget = forms.IntegerField(min_value=1000, label='Бюджет')
-    # add rest of the settings here later
+    cpm = forms.IntegerField(min_value=20, label='Ставка')
+    date_start = forms.DateField(
+        label='Дата старта',
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
+    date_end = forms.DateField(
+        label='Дата завершения',
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
+    frequency_cap = forms.IntegerField(label='Частота')
+    frequency_period = forms.IntegerField(label='Период частоты')
+    # # add rest of the settings here later
--- YandexDirect_API_Tool/create_campaign/views.py
+++ YandexDirect_API_Tool/create_campaign/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.forms import formset_factory
 
 from .forms import CampaignSettingsForm
 
@@ -7,17 +8,19 @@ import campaigns_create
 
 def index(request):
     template = 'index.html'
-    form = CampaignSettingsForm(request.GET or None)
-    if form.is_valid():
-        campaigns_create.CreateCampaign(
-            form.cleaned_data['campaign_name'],
-            60,
-            form.cleaned_data['total_budget'],
-            '2023-12-01',
-            '2023-12-05',
-            7,
-            30,
-        ).create_campaign()
-    context = {'form': form}
+    campaigns_formset = formset_factory(CampaignSettingsForm, extra=2)
+    formset = campaigns_formset(request.GET or None)
+    if formset.is_valid():
+        for form in formset:
+            campaigns_create.CreateCampaign(
+                form.cleaned_data['campaign_name'],
+                form.cleaned_data['cpm'],
+                form.cleaned_data['total_budget'],
+                str(form.cleaned_data['date_start']),
+                str(form.cleaned_data['date_end']),
+                form.cleaned_data['frequency_cap'],
+                form.cleaned_data['frequency_period'],
+            ).create_campaign()
+    context = {'formset': formset}
     return render(request, template, context)
 
